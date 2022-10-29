6b91682a9c9cbb25b810849aa6bc2d456ae6086d
Ilya Karas <ikaras@MacBook-Air-Ilya.local>
2022-10-29 15:35:40 +0300

    fixes task3 and task 4

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,12 +1,12 @@
 Ammount = int(input("Enter ammount of seconds: " ))
 Days = int(Ammount / 86400)
-Ammount = int(Ammount - Days * 86400)
+Ammount = (Ammount - Days * 86400)
 Hours = int(Ammount / 3600)
-Ammount = int(Ammount - Hours * 3600)
+Ammount = (Ammount - Hours * 3600)
 Minutes = int(Ammount / 60)
-Ammount = int(Ammount - Minutes * 60)
-Seconds = int(Ammount)
-print ("Days: " + str(Days) + "  Hours: " + str(Hours) + "   Minutes: " + str(Minutes) + "   Secounds: " +str(Seconds))
+Ammount = (Ammount - Minutes * 60)
+Seconds = (Ammount)
+print ("Days: " + str(Days) + "  Hours: " + str(Hours) + "   Minutes: " + str(Minutes) + "   Seconds: " +str(Seconds), end="")
 
 
 
--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -1,6 +1,11 @@
-a = float(input("Type a: "))
-b = float(input("Type b: "))
-c = float(input("Type c: "))
+a = int(input("Type a: "))
+b = int(input("Type b: "))
+c = int(input("Type c: "))
 D = float(b * b - 4 * a * c)
-print ("x1= " + str((- b + D ** 0.5) / 2 * a))
-print ("x1= " + str((- b - D ** 0.5) / 2 * a))
+if D > 0 :
+    print ("x1= " + str(( -b + (D ** 0.5)) / (2 * a)))
+    print ("x2= " + str(( -b - (D ** 0.5)) / (2 * a)), end="")
+if D == 0 :
+    print ("x= " + str(-b / (2 * a)), end="")
+if D < 0 :
+    print ("There is no solution", end="")
