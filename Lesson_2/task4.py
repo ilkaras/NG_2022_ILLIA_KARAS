4e48e4106bc8e7e959f768b8c4253acad96cf451
Ilya Karas <ikaras@MacBook-Air-Ilya.local>
2022-10-31 18:45:21 +0200

    less2.task4

--- Lesson_2/task4.py
+++ Lesson_2/task4.py
@@ -1,7 +1,7 @@
-a = int(input())
-b = 0
-while a > b: 
-    b += 1
-    print(b)
-    b = b * b
-    
+Number = int(input())
+dobutok = 1
+faktorial = 1
+while dobutok < Number:
+    faktorial += 1 
+    dobutok = dobutok * faktorial#1*2=2  2*3=6  3*4=12            
+print (faktorial)   
