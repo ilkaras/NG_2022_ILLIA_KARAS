57eb2f2d5095cb0ee40842baef921d66a7cc89f1
Ilya Karas <ikaras@MacBook-Air-Ilya.local>
2022-11-18 15:53:01 +0200

    task from we know changed

--- We know/dumb_decryptor.py
+++ We know/dumb_decryptor.py
@@ -1,12 +1,16 @@
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
 text = input("Input your text: ")
-ChangeOrder = int(input("Enter change order: "))
-transferedText = " "
-for element in text:
-    stockOrder = alphabet.find(element)
-    NewText = stockOrder + ChangeOrder
-    if element in alphabet:
-        transferedText += alphabet[NewText]
-    else:
-        transferedText += element
-print("Translated text: " + "\n" + str(transferedText))
+ChangeOrder = 25
+while ChangeOrder > 0: 
+    transferedText = " "
+    for element in text:
+        stockOrder = alphabet.find(element)
+        NewText = stockOrder + ChangeOrder
+        if element in alphabet:
+            transferedText += alphabet[NewText]
+        else:
+            transferedText += element
+    print("Rot" + str(ChangeOrder) +  "\n" + str(transferedText))
+    ChangeOrder = ChangeOrder - 1
+
+    
