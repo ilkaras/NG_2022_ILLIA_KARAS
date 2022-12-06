1ccc1a40399de75c609dadd9610f8b36d47b9e76
Ilya Karas <ikaras@MacBook-Air-Ilya.local>
2022-12-07 00:51:06 +0200

    Dumb decryptor fixed

--- We know/dumb_decryptor.py
+++ We know/dumb_decryptor.py
@@ -1,16 +1,21 @@
-alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+alphabet = ["abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+alphabetForDec = (alphabet[0]*2, alphabet[1]*2)
 text = input("Input your text: ")
-ChangeOrder = 25
-while ChangeOrder > 0: 
-    transferedText = " "
-    for element in text:
-        stockOrder = alphabet.find(element)
-        NewText = stockOrder + ChangeOrder
-        if element in alphabet:
-            transferedText += alphabet[NewText]
+Rot = len(alphabet[0])
+while Rot > 0: 
+    transferedText = ""
+    for element in text:  
+        if element in alphabetForDec[0]:
+            stockOrder = alphabetForDec[0].find(element)
+            NewText = stockOrder + Rot
+            transferedText += alphabetForDec[0][NewText]
+        elif element in alphabetForDec[1]:
+            stockOrder = alphabetForDec[1].find(element)
+            NewText = stockOrder + Rot
+            transferedText += alphabetForDec[1][NewText]
         else:
             transferedText += element
-    print("Rot" + str(ChangeOrder) +  "\n" + str(transferedText))
-    ChangeOrder = ChangeOrder - 1
+    print("Rot" + str(Rot) +  "\n" + str(transferedText))
+    Rot = Rot - 1
 
     
