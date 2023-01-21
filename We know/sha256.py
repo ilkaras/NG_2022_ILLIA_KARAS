ad8aa12f024d7274cde453da5b6b0999f172bc68
ilkaras <karas.ilya@ukr.net>
2023-01-21 13:20:13 +0100

    We know sha256 added

---
+++ We know/sha256.py
@@ -0,0 +1,21 @@
+from hashlib import sha256
+import itertools
+
+def decryptor(string,length,Hash):
+    if len(Hash) == 64:
+        for element in itertools.product(string,repeat=length):
+            passwd="".join(element)
+            if Hash == sha256(passwd.encode()).hexdigest():
+                print(Hash+" : "+ passwd)
+                quit()
+        length+=1
+    else:
+        print("Something wrong with your hash, try againg pleas!")
+
+def main():
+    Hash= input("Paste your hash: ")
+    length = 1
+    string="abcdefghijklmnopqrstuvwxyz1234567890 "
+    decryptor(string,length,Hash)
+
+main()
